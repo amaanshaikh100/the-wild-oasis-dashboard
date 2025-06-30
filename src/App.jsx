9f509bb540fa98578a9d0efca064696a67b43793
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-06-30 19:59:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: red;
+`;
+
+function App() {
+  return <H1>Hello world</H1>;
+}
+
+export default App;
