899cca8734205c4c3eedbaf145e749e382732625
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-07-03 01:04:27 +0530

    added form to create cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -6,6 +6,53 @@ export async function getCabins() {
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  // https://ilijfubkbxkhrupjkymx.supabase.co/storage/v1/object/public/cabin-images//cabin-003.jpg
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Create cabin
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }])
+    .select();
+
+  console.log(imagePath);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be created");
+  }
+
+  // 2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin IF there was an error
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error("Image could not be uploaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin deleted");
   }
 
   return data;
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ilijfubkbxkhrupjkymx.supabase.co";
+export const supabaseUrl = "https://ilijfubkbxkhrupjkymx.supabase.co";
 
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsaWpmdWJrYnhraHJ1cGpreW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzMTA2MzIsImV4cCI6MjA2Njg4NjYzMn0.W3BBoDfpBk0qQXiNCMUbHMbDB295B8glbNuYi6xxCqU";
